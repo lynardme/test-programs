0f8c3c6f3d77dcc291a3253933268ce0db77cd0f
Lynard <lynardsalingujay@outlook.com>
2019-10-14 14:04:51 +1300

    initial

---
+++ Divisible.java
@@ -0,0 +1,29 @@
+package orion;
+
+public class Divisible {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Divisible by 3");
+		for(int i = 1; i <= 100; i++) {
+			if(i % 3 == 0) {
+				System.out.print(i + " ");
+			}
+		}
+		System.out.println();
+		System.out.println("Divisible by 5");
+		for(int i = 1; i <= 100; i++) {
+			if(i % 5 == 0) {
+				System.out.print(i + " ");
+			}
+		}
+		System.out.println();
+		System.out.println("Divisible both 3 and 5");
+		for(int i = 1; i <= 100; i++) {
+			if((i % 3 == 0) && (i % 5 == 0)) {
+				System.out.print(i + " ");
+			}
+		}
+		
+	}
+}
